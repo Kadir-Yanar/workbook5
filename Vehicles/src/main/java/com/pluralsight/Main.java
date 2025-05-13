63848aa6c2021d9aa78017d2cb6d9e703d891a16
Abdulkadir Yanar <abdulkadiryanar03@gmail.com>
2025-05-13 08:47:01 -0700

    Main
    updated main

--- Vehicles/src/main/java/com/pluralsight/Main.java
+++ Vehicles/src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+        Car car1 = new Car("car","tesla","model3",4,10,50,224);
         System.out.println("Hello, World!");
     }
 }
