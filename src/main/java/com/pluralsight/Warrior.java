bfadab7793a21ea53bf1d09f99acb96d1d5f7edd
Abdulkadir Yanar <abdulkadiryanar03@gmail.com>
2025-05-13 08:50:43 -0700

    init commit

---
+++ src/main/java/com/pluralsight/Warrior.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Warrior extends Character {
+
+    public Warrior(String name, int hp, int attack) {
+        super(name, hp, attack);
+    }
+
+    void hpSpinAttack(Character target) {
+        System.out.println(name + " unleashes HP SPINNING ATTACK! 🌀 " + target.name + " is wiped out instantly!");
+        target.hp = 0;
+    }
+}
